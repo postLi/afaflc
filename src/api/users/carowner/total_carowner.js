5705328f34dbad83d6bfb36bd792c8ae7e8ccc35
zenglipeng <1209782757@qq.com>
2018-08-14 13:40:30 +0800

    -huozhu1

--- src/api/users/carowner/total_carowner.js
+++ src/api/users/carowner/total_carowner.js
@@ -97,5 +97,5 @@ export function data_get_shipper_auid() {
 
 // 根据身份证获取车主
 export function data_post_checkDriverCardid(data) {
-  return fetch.get('/' + baseurl_four + '/usercenter/aflcDriver/v1/checkDriverCardid/' + data, data)
+  return fetch.get('/' + baseurl + '/usercenter/aflcDriver/v1/checkDriverCardid/' + data, data)
 }
