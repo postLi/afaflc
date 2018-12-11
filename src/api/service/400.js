a57ab52b0dbfb111e9d6d52fd2bbadf41df9ce88
Zhousii <tzjz6065278@163.com>
2018-12-11 19:17:13 +0800

    400播放和下载

--- src/api/service/400.js
+++ src/api/service/400.js
@@ -1,6 +1,8 @@
 import fetch from '@/utils/fetch'
 
 const baseurl = "aflc-common"
+// const baseurl = "aflccommonservice-lyc"
+
 
 //根据条件获取400通话记录表列表
 export function aflcCallLog(page,pagesize,data) {
@@ -12,5 +14,14 @@ export function aflcCallLog(page,pagesize,data) {
         "pageSize": pagesize,
         "vo": data
       }
+    })
+  }
+
+
+//获取400通话记录链接
+export function getVoiceUrl(Ish,date) {
+    return fetch({
+      url: '/'+ baseurl + '/common/aflcCallLog/v1/getVoiceUrl/'+Ish+'?date='+date,
+      method: 'get',
     })
   }
--- src/views/sm/QRCode/newQRCode.vue
+++ src/views/sm/QRCode/newQRCode.vue
@@ -10,7 +10,7 @@
                     </el-input>
                 </el-form-item>
                 <el-form-item label="渠道名称"   prop="channalName">
-                    <el-input v-model="searchInfo.channalName" clearable placeholder="账户/姓名">
+                    <el-input v-model="searchInfo.channalName" clearable>
                     </el-input>
                 </el-form-item>
                 <el-form-item class="btnChoose fr"  style="margin-left:0;">
